6dcbf843f1cd7b389ebb7e8973448e7df1133840
Fenrir7734 <hitex1999@gmail.com>
2022-06-17 17:44:44 +0200

    Sound reactive LED strip

---
+++ Sound_reactive_LED_strip/src/main.cpp
@@ -0,0 +1,34 @@
+#include <FastLED.h>
+
+#define LED_DATA_PIN 13
+#define MICROPHONE_PIN A5
+#define LED_NUM 8
+#define LED_MAX 255 * LED_NUM
+
+CRGB leds[LED_NUM];
+
+void setup() {
+    FastLED.addLeds<WS2812 ,LED_DATA_PIN, GRB>(leds, LED_NUM);
+}
+
+void loop() {
+  int read = analogRead(MICROPHONE_PIN);
+  int level = (2048. / 550.) * read;
+
+  if (level != 0) {
+    int i = 0;
+    while (i < LED_NUM) {
+      if (level >= 255) {
+        leds[i] = CRGB(0, 0, 255);
+        level -= 255;
+      } else if (level > 0 && level < 255) {
+        leds[i] = CRGB(0, 0, level);
+        level -= 255;
+      } else {
+        leds[i] = CRGB(0, 0, 0);
+      }
+      i++;
+    }
+    FastLED.show();
+  } 
+}
