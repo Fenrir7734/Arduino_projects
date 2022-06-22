73de56eda939d259f1211fbeb219568f4dfa3f69
Fenrir7734 <hitex1999@gmail.com>
2022-06-22 22:03:58 +0200

    sscanf replaced by custom solution

--- Bluetooth_controled_LED/RGB_LED_color_changing/src/main.cpp
+++ Bluetooth_controled_LED/RGB_LED_color_changing/src/main.cpp
@@ -16,7 +16,36 @@ SoftwareSerial HC06(RX_PIN, TX_PIN);
 
 uint8_t started = 1;
 char hex[6];
-int index = 0;
+uint8_t index = 0;
+
+unsigned long pow(int a, int b) {
+  unsigned long result = 1;
+
+  for (int i = 0; i < b; i++) {
+    result *= a;
+  }
+  return result;
+}
+
+unsigned long hexToDec(char* arr, uint8_t size) {
+  unsigned long dec = 0;
+
+  for (uint8_t i = 0; i < size; i++) {
+    char curr = arr[size - i - 1];
+    
+    if (curr >= 48 && curr <= 57) {
+      curr -= 48;
+    } else if (curr >= 65 && curr <= 70) {
+      curr = (curr - 65) + 10;
+    } else if (curr >= 97 && curr <= 102) {
+      curr = (curr - 97) + 10;
+    }
+
+    dec += (curr * pow(16, i));
+  }
+
+  return dec;
+}
 
 int normalizeColorValue(int value) {
   if (COMMON_ANODE) {
@@ -26,11 +55,12 @@ int normalizeColorValue(int value) {
   }
 }
 
-void setColor() {
-  int red, green, blue;
+void setColor(char* hex) {
+  unsigned long dec = hexToDec(hex, index);
 
-  // It should be replaced by something cheaper
-  sscanf(hex, "%02x%02x%02x", &red, &green, &blue);
+  int red = (dec >> 16) & 0xFF;
+  int green = (dec >> 8) & 0xFF;
+  int blue = dec & 0xFF;
 
   analogWrite(LED_RED_PIN, normalizeColorValue(red));
   analogWrite(LED_GREEN_PIN, normalizeColorValue(green));
@@ -50,7 +80,7 @@ void loop() {
       index = 0;
     } else if (value == END_SIGN) {
       started = 0;
-      setColor();
+      setColor(hex);
     } else if (started && index < 6) {
       hex[index++] = (char) value;
     } else {
